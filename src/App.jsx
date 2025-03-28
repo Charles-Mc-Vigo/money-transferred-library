e02c7fbc2bdf8e369397b1a91a4d1c1e240dc270
CharlesVigo <charlesvigo8@gmail.com>
2025-03-28 19:11:54 +0800

    layout added. component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import TransactionTable from "./components/TransactionTable";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
 
 function App() {
-	return (
-		<>
-			<Navbar />
-		</>
-	);
+  return (
+    <div className="h-screen flex flex-col">
+      {/* Navbar */}
+      <div className="h-auto bg-white shadow-md">
+        <Navbar />
+      </div>
+
+      <div className="flex flex-1 overflow-hidden">
+        {/* Sidebar */}
+        <div className="w-1/4 bg-gray-200 p-4">
+          <Sidebar />
+        </div>
+
+        {/* Main Content */}
+        <div className="flex-1 p-6 overflow-auto bg-gray-100">
+          <TransactionTable />
+        </div>
+      </div>
+
+      {/* Footer */}
+      <div className="h-12 bg-gray-300 text-center flex items-center justify-center">
+        <Footer />
+      </div>
+    </div>
+  );
 }
+
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ function classNames(...classes) {
 export default function Navbar() {
 	return (
 		<Disclosure as="nav" className="bg-blue-600">
-			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 p-3">
 				<div className="relative flex h-16 items-center justify-between">
 					{/* Mobile menu button */}
 					<div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
